3f34ef07f50958a873a2b28e4cb39a34f3bd57de
marianneporter <mariannneporter17@gmail.com>
2023-02-21 11:07:10 +0000

    wip js changes

--- resources/js/cats/create-update.js
+++ resources/js/cats/create-update.js
@@ -1,6 +1,6 @@
 import 'flowbite-datepicker'
 import 'flowbite/dist/datepicker.js';
-import Datepicker from 'flowbite-datepicker/Datepicker';
+import  FlowbiteDatepicker from 'flowbite-datepicker/Datepicker';
 
 import { dispDateToYYYYMMDD } from "../../js/dates";
 
@@ -22,7 +22,7 @@ export const createDatepicker = (datepickerEl) => {
         const yearsAgo30 = today.getFullYear() - 30;        
         const minDate = new Date(yearsAgo30, 0, 1)
         
-        new Datepicker(datepickerEl, {   
+        new FlowbiteDatepicker(datepickerEl, {   
             maxDate: new Date(),
             minDate: minDate,
             format: 'd M yyyy',
